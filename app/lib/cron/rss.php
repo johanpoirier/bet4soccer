3e6b44d9cc05c2d53f43c370ff560d9bb8e5aff9
Johan Poirier <johan.poirier@gmail.com>
2016-05-27 14:51:21 +0200

    RSS fetch UTF-8 style

--- app/lib/cron/rss.php
+++ app/lib/cron/rss.php
@@ -15,21 +15,24 @@ $bet = new BetEngine(false, false);
 
 $feedIo = \FeedIo\Factory::create()->getFeedIo();
 $rss = $feedIo->read($bet->config['rss_feed_url']);
-$regexp = sprintf('/^$s : ([a-zA-Z\- ]*) - ([a-zA-Z\- ]*) \(score final : ([0-9])-([0-9])/', $bet->config['rss_feed_title']);
+$regexp = sprintf('/^%s : ([\w \']*) - ([\w \']*) \(score final ([\d]*)-([\d]*)\)/u', $bet->config['rss_feed_title']);
 
 foreach ($rss->getFeed() as $item) {
     $content = $item->getTitle();
     if (preg_match($regexp, $content, $vars)) {
-        $match = $bet->matches->get_by_team_names($vars[1], $vars[2]);
+        if ($simulation === true) {
+            echo trim($vars[1]) . ' vs ' . trim($vars[2]) . ': ' . $vars[3] . '-' . $vars[4] . "\n";
+        }
+        $match = $bet->matches->get_by_team_names(trim($vars[1]), trim($vars[2]));
         if($match) {
             if($match['scoreMatchA'] == null) {
-                echo "engine->games->saveResult(" . $match['matchID'] . ", " . $match['teamAid'] . ", ". $vars[3] . ");\n";
+                echo '$bet->matches->saveResult(' . $match['matchID'] . ', ' . $match['teamAid'] . ', '. $vars[3] . ");\n";
                 if ($simulation === false) {
                     $bet->matches->saveResult($match['matchID'], $match['teamAid'], $match[3]);
                 }
             }
             if($match['scoreMatchB'] == null) {
-                echo "engine->games->saveResult(" . $match['matchID'] . ", " . $match['teamBid'] . ", ". $vars[4] . ");\n";
+                echo '$bet->matches->saveResult(' . $match['matchID'] . ', ' . $match['teamBid'] . ', '. $vars[4] . ");\n";
                 if ($simulation === false) {
                     $bet->matches->saveResult($match['matchID'], $match['teamBid'], $match[4]);
                 }
