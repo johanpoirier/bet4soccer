b088f0039a1712fe03c0a94c92c4ca78cbeb636c
Johan Poirier <johan.poirier@gmail.com>
2016-05-30 14:58:45 +0200

    Switch current version at the end of deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,17 +9,24 @@ if [ -e $targetPath ]
 then
     version=`date '+%Y%m%d%H%M%S'`
     mkdir -p "$targetPath/versions/$version/web/"
-    rm -f "$targetPath/current"
-    ln -s "$targetPath/versions/$version/" "$targetPath/current"
 
     cd "$projectDir"
-    cp -r app/* "$targetPath/current/web/"
-    mkdir "$targetPath/current/logs"
-    chmod 775 "$targetPath/current/logs"
-    cp "$targetPath/conf/config.inc.php" "$targetPath/current/web/lib/config.inc.php"
-    cp "$targetPath/conf/params.inc" "$targetPath/current/web/lib/protect/params.inc"
-    chgrp -R www-data "$targetPath/current/"
+    cp -r app/* "$targetPath/versions/$version/web/"
+
+    mkdir "$targetPath/versions/$version/logs"
+    chmod 775 "$targetPath/versions/$version/logs"
+
+    rm -f "$targetPath/versions/$version/web/lib/config.inc.php"
+    ln -s "$targetPath/conf/config.inc.php" "$targetPath/versions/$version/web/lib/config.inc.php"
+
+    rm -f "$targetPath/versions/$version/web/lib/protect/params.inc"
+    ln -s "$targetPath/conf/params.inc" "$targetPath/versions/$version/web/lib/protect/params.inc"
+
+    chgrp -R www-data "$targetPath/versions/$version/"
     cd --
+
+    rm -f "$targetPath/current"
+    ln -s "$targetPath/versions/$version/" "$targetPath/current"
 
     echo "Version $version created"
 else
